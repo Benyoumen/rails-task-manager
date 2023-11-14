5f0d439ed7291f04d4a6bb63fba98db10a42b963
Benjamin Tayar <tayarbenjamin@gmail.com>
2023-11-14 16:37:48 -0300

    solved

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+// Add checked class when clicked
+document.querySelector('.form-check-input').addEventListener('click', () => {
+  document.querySelector('.task-details').classList.toggle('checked');
+})
